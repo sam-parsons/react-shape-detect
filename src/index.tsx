2aca00298d972fcad09ce42a07b45a018df32704
Sam Parsons <samparsons269@gmail.com>
2021-01-03 13:31:23 -0800

    modularized ShapeDetect component

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,6 @@
 import React from 'react';
-import { ShapeDetectProps, ShapeDetectState } from './types';
+import ShapeDetect from './components/ShapeDetect';
 
-export default class ShapeDetect extends React.Component<ShapeDetectProps, ShapeDetectState> {
-  constructor(props: ShapeDetectProps) {
-    super(props);
-  }
-
-  async detect(event: any) {
-    const target = event.target;
-
-    if (!('FaceDetector' in window)) {
-      alert('Your browser doesn\'t support the Shape Detection API');
-    }
-
-    // @ts-ignore
-    const faceDetect = new FaceDetector({
-      maxDetectedFaces: 5,
-      fastMode: false
-    });
-    
-    try {
-      const result = await faceDetect.detect(target);
-      console.log('result', result);
-    } catch (e) {
-      console.error(e);
-      alert('Detection error: ' + e.message);
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <img 
-          id={'shape-detect-99'} 
-          src={this.props.image} 
-          onLoad={(e) => this.detect(e)} 
-          crossOrigin={'anonymous'}
-        />
-      </div>
-    );
-  }
+export default function(props: any) {
+  return <ShapeDetect image={props.image} onRender={props.onRender} />;
 }
