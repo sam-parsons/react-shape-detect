2cc00bde2e3c87f549ac382f305bea5dddd8c60c
Sam Parsons <samparsons269@gmail.com>
2021-01-03 13:11:30 -0800

    update image path in test file

--- __tests__/ShapeDetect.tsx
+++ __tests__/ShapeDetect.tsx
@@ -4,6 +4,6 @@ import { render } from '@testing-library/react';
 
 describe('ShapeDetect', () => {
   it('renders', () => {
-    render(<ShapeDetect image={'https://api.imgur.com/3/image/14fc8on'} onRender={data => console.log(data)} />);
+    render(<ShapeDetect image={"https://cdn.glitch.com/5a76353b-f724-4f28-b022-58fd035621f1%2F1280px-Schmidt-Brin-Page-20080520.jpg?1547043906122"} onRender={data => console.log(data)} />);
   });
 });
