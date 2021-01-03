6807310e97e214584bbf4c68ae7a1fa341bc9a5f
Sam Parsons <samparsons269@gmail.com>
2021-01-02 22:35:49 -0800

    initial set up

---
+++ __tests__/ShapeDetect.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ShapeDetect from '../src/index';
+import { render } from '@testing-library/react';
+
+describe('ShapeDetect', () => {
+  
+  it('renders', () => {
+    render(<ShapeDetect />);
+  });
+
+})
