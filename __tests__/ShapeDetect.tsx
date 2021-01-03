c8cffd9ddef364fe0d467faab2eb7f8188ef280e
Sam Parsons <samparsons269@gmail.com>
2021-01-03 14:31:46 -0800

    added options attribute to component

--- __tests__/ShapeDetect.tsx
+++ __tests__/ShapeDetect.tsx
@@ -4,6 +4,12 @@ import { render } from '@testing-library/react';
 
 describe('ShapeDetect', () => {
   it('renders', () => {
-    render(<ShapeDetect image={"https://cdn.glitch.com/5a76353b-f724-4f28-b022-58fd035621f1%2F1280px-Schmidt-Brin-Page-20080520.jpg?1547043906122"} onRender={data => console.log(data)} />);
+    render(
+      <ShapeDetect 
+        image={"https://cdn.glitch.com/5a76353b-f724-4f28-b022-58fd035621f1%2F1280px-Schmidt-Brin-Page-20080520.jpg?1547043906122"} 
+        onRender={data => console.log(data)} 
+        options={{ type: 'face' }}
+      />
+    );
   });
 });
