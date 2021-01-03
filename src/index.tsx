59670be120a55e35628eccd1627462fc43708243
Sam Parsons <samparsons269@gmail.com>
2021-01-03 13:32:28 -0800

    typing and versioning updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ShapeDetect from './components/ShapeDetect';
+import { ShapeDetectProps } from './types';
 
-export default function(props: any) {
+export default function(props: ShapeDetectProps) {
   return <ShapeDetect image={props.image} onRender={props.onRender} />;
 }
