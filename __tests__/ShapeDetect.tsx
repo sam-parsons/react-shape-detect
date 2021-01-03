ed871f01bfba9754495162be05faf20051fc4309
Sam Parsons <samparsons269@gmail.com>
2021-01-03 10:10:45 -0800

    added attributes to render test

--- __tests__/ShapeDetect.tsx
+++ __tests__/ShapeDetect.tsx
@@ -3,9 +3,7 @@ import ShapeDetect from '../src/index';
 import { render } from '@testing-library/react';
 
 describe('ShapeDetect', () => {
-  
   it('renders', () => {
-    render(<ShapeDetect />);
+    render(<ShapeDetect image={'https://api.imgur.com/3/image/14fc8on'} onRender={data => console.log(data)} />);
   });
-
-})
+});
