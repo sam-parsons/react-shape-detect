a7b252dbd4b86ba46d1ad821c5f0b7bd727feb8c
Sam Parsons <samparsons269@gmail.com>
2021-01-03 20:13:15 -0800

    testing environment set up and src prop test

--- __tests__/ShapeDetect.tsx
+++ __tests__/ShapeDetect.tsx
@@ -1,10 +1,6 @@
 import React, { SyntheticEvent } from 'react';
 import ShapeDetect from '../src/index';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-// @ts-ignore
-Enzyme.configure({ adapter: new Adapter() });
+import { mount } from 'enzyme';
 
 const url = "https://cdn.glitch.com/5a76353b-f724-4f28-b022-58fd035621f1%2F1280px-Schmidt-Brin-Page-20080520.jpg?1547043906122"
 
@@ -23,7 +19,7 @@ describe('ShapeDetect', () => {
         options={{ type: 'face' }}
       />
     );
+    expect(wrapper.find('img').prop('src')).toEqual(url);
   });
-
 
 });
