088b080319dab164139f020894f0961accf0c256
Sam Parsons <samparsons269@gmail.com>
2021-01-03 18:09:47 -0800

    ShapeDetect is a functional component

--- src/components/ShapeDetect.tsx
+++ src/components/ShapeDetect.tsx
@@ -1,39 +1,39 @@
 import React, { SyntheticEvent } from 'react';
 import { ShapeDetectProps } from '../types';
-import detectFactory from '../util/DetectFactory';
+import detectorFactory from '../util/DetectFactory';
 
-export default class ShapeDetect extends React.Component<ShapeDetectProps> {
-  constructor(props: ShapeDetectProps) {
-    super(props);
-  }
+export default (props: ShapeDetectProps) => {
 
-  async detect(event: SyntheticEvent) {
-    if (!('FaceDetector' || 'BarcodeDetector' in window)) alert('Your browser doesn\'t support the Shape Detection API');
-    
-    const target = event.target;
+  const generateDetectCallback = (type: string, onRender: (data: any) => void) => {
+    return async function(event: SyntheticEvent) {
+      if (!('FaceDetector' || 'BarcodeDetector' in window)) 
+        alert('Your browser doesn\'t support the Shape Detection API');
+      
+      // save ImageBitmapSource
+      const target = event.target;
 
-    const detector = detectFactory(this.props.options?.type ?? '');
-    
-    try {
-      const result = await detector.detect(target)
-      this.props.onRender(result);
-      return result;
-    } catch (e) {
-      console.error(e);
-      alert('Detection error: ' + e.message);
+      const detector = detectorFactory(type ?? '');
+      
+      try {
+        const result = await detector.detect(target);
+        onRender(result);
+        return result;
+      } catch (e) {
+        console.error(e);
+        alert('Detection error: ' + e.message);
+      }
     }
-
   }
 
-  render() {
-    return (
-      <div>
-        <img 
-          src={this.props.image} 
-          onLoad={this.detect.bind(this)} 
-          crossOrigin={'anonymous'}
-        />
-      </div>
-    );
-  }
+  const onLoadCallback = generateDetectCallback(props.options?.type ?? '', props.onRender)
+
+  return (
+    <div>
+      <img 
+        src={props.image} 
+        onLoad={onLoadCallback} 
+        crossOrigin={'anonymous'}
+      />
+    </div>
+  );
 }
