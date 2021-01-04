8c7b4c905b1cb9a2dec947563e264b6b51d48430
Sam Parsons <samparsons269@gmail.com>
2021-01-03 18:16:27 -0800

    update onRender callback

--- __tests__/ShapeDetect.tsx
+++ __tests__/ShapeDetect.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import ShapeDetect from '../src/index';
 import { render } from '@testing-library/react';
 
 describe('ShapeDetect', () => {
-  const onRender = (data: {}[]) => {
-    expect(data.length === 4).toBe(true);
+  const onRender = () => {
+    return (data: SyntheticEvent) => {
+      return;
+    }
   }
 
   it('renders', () => {
     render(
       <ShapeDetect 
         image={"https://cdn.glitch.com/5a76353b-f724-4f28-b022-58fd035621f1%2F1280px-Schmidt-Brin-Page-20080520.jpg?1547043906122"} 
-        onRender={onRender} 
+        onRender={onRender()} 
         options={{ type: 'face' }}
       />
     );
