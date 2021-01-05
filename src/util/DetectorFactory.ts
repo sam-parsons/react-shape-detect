0904c2385a42a8775ecf89e402bbe06efdc6d9f2
Sam Parsons <samparsons269@gmail.com>
2021-01-04 16:54:43 -0800

    removed hardcoded facedetector options

--- src/util/DetectorFactory.ts
+++ src/util/DetectorFactory.ts
@@ -1,13 +1,11 @@
+// handle options input
 export default function(type: string) {
   let detect;
   
   switch(type) {
     case 'face':
       // @ts-ignore
-      detect = new FaceDetector({
-        maxDetectedFaces: 5,
-        fastMode: false
-      });
+      detect = new FaceDetector();
       break;
     case 'barcode':
       // @ts-ignore
