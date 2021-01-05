67ddf01f368a6c6dcaf729f9f625d89b2af6fa10
Sam Parsons <samparsons269@gmail.com>
2021-01-04 16:55:43 -0800

    ShapeDetect handles custom img tag attributes

--- src/components/ShapeDetect.tsx
+++ src/components/ShapeDetect.tsx
@@ -7,12 +7,12 @@ export default (props: ShapeDetectProps) => {
   const onLoadCallback = ((type: string, onRender: (data: any) => void) => {
     return async function(event: SyntheticEvent) {
       if (!('FaceDetector' || 'BarcodeDetector' in window)) 
-        alert('Your browser doesn\'t support the Shape Detection API');
+      alert('Your browser doesn\'t support the Shape Detection API');
+      
+      const detector = detectorFactory(type ?? '');
       
       const target = event.target;
 
-      const detector = detectorFactory(type ?? '');
-      
       try {
         const result = await detector.detect(target);
         onRender(result);
@@ -24,13 +24,20 @@ export default (props: ShapeDetectProps) => {
     }
   })(props.options?.type ?? '', props.onRender);
 
+  // spread custom img tag attributes
+  const imgProps: {[key: string]: any} = Object.assign({}, props.options?.attributes)
+  for (const prop in props.options?.attributes) {
+    // @ts-ignore
+    imgProps[prop] = props.options?.attributes[prop]
+  }
+  // assign source and callback then generate image element
+  imgProps.src = props.image
+  imgProps.onLoad = onLoadCallback;
+  const img = React.createElement('img', imgProps);
+
   return (
     <div>
-      <img 
-        src={props.image} 
-        onLoad={onLoadCallback} 
-        crossOrigin={'anonymous'}
-      />
+      {img}
     </div>
   );
 }
