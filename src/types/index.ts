4791e10a7c6b19df25e9d0d91fba6f319e004e07
Sam Parsons <samparsons269@gmail.com>
2021-01-04 16:53:43 -0800

    added attributes object on options prop

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,10 @@ import { SyntheticEvent } from "react";
 export type ShapeDetectProps = {
   image: string,
   options?: {
-    type?: string
+    type?: string,
+    attributes?: {
+      alt?: string
+    }
   },
   onRender: (data: SyntheticEvent) => void
 }
