f94c937cfa63ef83e858e3028fd3e318f15dfbfa
Sam Parsons <samparsons269@gmail.com>
2021-01-06 19:37:14 -0800

    component refactor

--- src/components/ShapeDetect.tsx
+++ src/components/ShapeDetect.tsx
@@ -1,17 +1,16 @@
-import React, { SyntheticEvent } from 'react';
+import React from 'react';
 import { ShapeDetectProps } from '../types';
 import detectImage from '../util/detectImage';
 
 export default (props: ShapeDetectProps) => {
 
-  const onLoadCallback: (data: SyntheticEvent) => void = detectImage(props.options?.type ?? '', props.onRender);
-
   // spread custom img tag attributes from public props
   // assign required attributes then generate image element
   const imgProps: {[key: string]: any} = Object.assign({}, props.options?.attributes)
   imgProps.src = props.image
-  imgProps.onLoad = onLoadCallback;
   imgProps.crossOrigin = 'anonymous';
+  // @ts-ignore
+  imgProps.onLoad = detectImage(props.options?.type ?? '', props.onRender);
 
   return React.createElement('img', imgProps);
 }
