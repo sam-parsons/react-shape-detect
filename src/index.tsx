df059e0b6d069986b648a78f04347aff86fd3c63
Sam Parsons <samparsons269@gmail.com>
2021-01-06 19:35:40 -0800

    vertical refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React, { SyntheticEvent } from 'react';
 import ShapeDetect from './components/ShapeDetect';
 import { ModuleProps } from './types';
+import mockOnRender from './util/mockOnRender';
 
 export default function(props: ModuleProps) {
-  const mockOnRender = (data: SyntheticEvent) => {};
-  return <ShapeDetect image={props.image} onRender={props.onRender ?? mockOnRender} options={props.options} />;
+  return (
+    <ShapeDetect 
+      image={props.image} 
+      onRender={props.onRender ?? mockOnRender} 
+      options={props.options} 
+    />
+  );
 }
