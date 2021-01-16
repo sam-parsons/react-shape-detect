4723d47b6b3dcd5b9d253b365cdd6c89731b9f21
Sam Parsons <samparsons269@gmail.com>
2021-01-16 14:25:32 -0800

    beginnings of overlay functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ShapeDetect from './components/ShapeDetect';
-import { ModuleProps } from './types';
+import { ModuleProps, ModuleState } from './types';
 import mockOnRender from './util/mockOnRender';
 
-export default function(props: ModuleProps) {
-  return (
-    <ShapeDetect 
-      image={props.image} 
-      onRender={props.onRender ?? mockOnRender} 
-      options={props.options} 
-    />
-  );
+export default class Module extends React.Component<ModuleProps, ModuleState> {
+
+  constructor(props: ModuleProps) {
+    super(props);
+
+    this.state = {
+      componentData: [],
+      input: props.options?.overlay?.input
+    }
+  }
+  
+  newOnRender(onRender: any) {
+    const that = this;
+    return function(data: any) {
+      that.setState({componentData: data}, () => {
+        onRender(data);
+      });
+    }
+  }
+
+  render() {
+    const component = this.state.input
+
+    return (
+      <React.Fragment>
+        <ShapeDetect 
+          image={this.props.image} 
+          onRender={this.props.onRender ? this.newOnRender(this.props.onRender) : mockOnRender} 
+          options={this.props.options} 
+        />
+        {this.state.componentData.map(data => {
+          return component
+        })}
+      </React.Fragment>
+    );
+  }
 }
