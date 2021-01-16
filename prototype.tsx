16e1c53f2f1645cafbbb43961c350fd5bd8d3f50
Sam Parsons <samparsons269@gmail.com>
2021-01-16 14:26:18 -0800

    prototype update

--- prototype.tsx
+++ prototype.tsx
@@ -5,10 +5,10 @@
     type: 'face', // 'barcode', 'text'
     projection: {}, // specify what attributes will exist in onRender parameter
     overlay: {
-      type: 'default', // overridden if overlay attribute has a value // v0.1
+      type: 'default', // overridden if overlay attribute has a value // v0.2
       color: 'white', // other common discreet colors
       scale: 'fit-to-overlay', // accept pixels '240px 180px' or percentages '90%'
-      input: component // isomorphic? can accept component or image path // v0.2
+      input: component // isomorphic? can accept component or image path // v0.3
     }
   }}
   onRender={data => this.setState(data)} // v0.1
