140fdd0e6da34e663b930c2dd3b0ce81a1744b79
Sam Parsons <samparsons269@gmail.com>
2021-01-17 15:56:27 -0800

    custom id on imgProps

--- src/components/ShapeDetect.ts
+++ src/components/ShapeDetect.ts
@@ -17,6 +17,7 @@ export default (props: ShapeDetectProps) => {
     height: 'auto',
     width: props.options?.attributes?.width ?? `${props.options?.overlay?.scale}%`
   };
+  imgProps.id = 'shape-detect-' + props.image.slice(-8);
   // @ts-ignore
   imgProps.onLoad = detectImage(props.options?.type ?? '', props.onRender);
 
