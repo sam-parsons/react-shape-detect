76e3d3073b2765d527505a9080d50814f061330b
Sam Parsons <samparsons269@gmail.com>
2021-01-17 12:38:56 -0800

    scale processing for detection data

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -4,16 +4,22 @@ import { OverlayProps } from '../types'
 export default (props: OverlayProps): JSX.Element => {
 
   const svgs = props.componentData.map((data: any, key: number): ReactElement => {
-    const { x, y, height, width } = data.boundingBox;
+    const keys = ['x', 'y', 'height', 'width'];
+    const newObj: any = {}
+    keys.forEach(key => {
+      // @ts-ignore
+      newObj[key] = data.boundingBox[key] * (props.componentAttributes?.scale * 0.01 ?? 1)
+    })
+    const { x, y, height, width } = newObj;
     return React.createElement('rect', { x, y, height, width, key });
   });
 
   return (
     <svg 
       style={{
-      position: 'absolute',
-      top: 0,
-      left: 0
+        position: 'absolute',
+        top: 0,
+        left: 0
       }}
     >
       {svgs}
