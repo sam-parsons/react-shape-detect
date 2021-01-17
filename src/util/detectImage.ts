76d1a8d3f0239ed8cc440497508e0c8ac8874dd5
Sam Parsons <samparsons269@gmail.com>
2021-01-17 15:15:01 -0800

    onRender argument refactor

--- src/util/detectImage.ts
+++ src/util/detectImage.ts
@@ -1,13 +1,15 @@
 import { SyntheticEvent } from 'react';
 import detectorFactory from './DetectorFactory';
 
-export default (type: string, onRender: (data: any) => void) => {
+export default (type: string, onRender: (data: any, imageData: any) => void) => {
   return async function(event: SyntheticEvent) {
     try {
       const detector = detectorFactory(type ?? '');      
-      const target = event.target;
-      const result = await detector.detect(target);
-      onRender(result);
+      const image = event.target as HTMLImageElement;
+      const { naturalHeight, naturalWidth, height, width } = image;
+      const imageData = { naturalHeight, naturalWidth, height, width };
+      const result = await detector.detect(image);
+      onRender(result, imageData);
       return result;
     } catch (e) {
       console.error(e);
