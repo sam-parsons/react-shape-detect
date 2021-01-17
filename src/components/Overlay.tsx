14ec294de48f7da2d5beb3eaf19cff5d7fcb746d
Sam Parsons <samparsons269@gmail.com>
2021-01-17 15:12:15 -0800

    SVG scaling with both width and scale props

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,14 +1,25 @@
 import React, { ReactElement } from 'react';
-import { OverlayProps } from '../types'
+import { OverlayProps } from '../types';
+import Pixel from '../util/pixel';
 
 export default (props: OverlayProps): JSX.Element => {
+  const keys = ['x', 'y', 'height', 'width'];
+  // if absolute width is found, then provide scale for data translation
+  // otherwise if overlay scale attribute provided, produce scaled data
+  // if not either, image size adultered
+  let svgs;
+  let scale = 1;
+  if (props.imageData?.width !== props.imageData?.naturalWidth) {
+    scale = Pixel.computeScale(props.imageData); // return 0.5 for 50%
+  } else if (props.componentAttributes?.scale) {
+    scale = (props.componentAttributes?.scale * 0.01 ?? 1)
+  } 
 
-  const svgs = props.componentData.map((data: any, key: number): ReactElement => {
-    const keys = ['x', 'y', 'height', 'width'];
-    const newObj: any = {}
+  svgs = props.componentData.map((data: any, key: number): ReactElement => {
+    const newObj: any = {};
     keys.forEach(key => {
       // @ts-ignore
-      newObj[key] = data.boundingBox[key] * (props.componentAttributes?.scale * 0.01 ?? 1)
+      newObj[key] = data.boundingBox[key] * scale; 
     })
     const { x, y, height, width } = newObj;
     return React.createElement('rect', { x, y, height, width, key });
